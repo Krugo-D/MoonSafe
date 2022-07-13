0cba031bd56b52378d9d01c546c8bc7097c9c012
dogperson <fondofdogs@protonmail.com>
2022-07-13 13:06:14 +0700

    add liq gives transfer help err

--- scripts/add-liquidity.js
+++ scripts/add-liquidity.js
@@ -5,20 +5,19 @@ async function main() {
   // get signer
   const [owner] = await ethers.getSigners();
 
+  // WETH address on mainnet
+  const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+
   // We get the deployed MoonSafe contract
-  const moonSafe = await hre.ethers.getContractAt("MoonSafe", "0x2dd2812b07B59Cb89E8077c6CB42c6D6ee08ca12");
-  const routerAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-  const uRouter = await hre.ethers.getContractAt("IUniswapV2Router02", routerAddress);
+  const moonSafe = await hre.ethers.getContractAt("MoonSafe", '0x2dd2812b07B59Cb89E8077c6CB42c6D6ee08ca12');
+  const routerAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+  const uRouter = await hre.ethers.getContractAt('IUniswapV2Router02', routerAddress);
   const router = uRouter.connect(owner)
 
-  // get uniswap LP pair instance
-  const pairAddress = await moonSafe.uniswapV2Pair();
-  const pair = await hre.ethers.getContractAt("IUniswapV2Pair", pairAddress);
-
-  // approve router and pair to use max amount of tokens from owner's wallet
+  // approve router to use max amount of tokens and WETH from owner's wallet
   const max = (2^256 - 1);
-  await moonSafe.connect(owner).approve(router.address, max);
-  await moonSafe.connect(owner).approve(pair.address, max);
+  await moonSafe.connect(owner).approve(routerAddress, max);
+  await moonSafe.connect(owner).approve(wethAddress, max);
 
   // approve router to use max amount of WETH from owner's wallet
 
@@ -33,7 +32,7 @@ async function main() {
     moonSafe.address,
     tokensToAdd,
     0, // slippage is unavoidable
-    ethToAdd, // slippage is unavoidable
+    0, // slippage is unavoidable
     owner.address,
     (Date.now() + 100000),
     { value: ethers.utils.parseUnits("1", "ether") }
@@ -46,6 +45,8 @@ async function main() {
   console.log(`tokenbalance after: ${tokenBalanceAfter}`);
 
   // check if owner has received LP tokens in exchange for tokens
+  const pairAddress = await moonSafe.uniswapV2Pair();
+  const pair = await hre.ethers.getContractAt('IUniswapV2Pair', pairAddress);
   const lpBalance = await pair.balanceOf(owner.address)
   console.log(`lp balance after: ${lpBalance}`);
 }
